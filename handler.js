b614e9b11cc79a2edaaa6992776b66ca4a05ec5a
Jonathan Morales <jonmorazav@yahoo.com>
2019-11-05 15:36:27 -0500

    add completed lambda function

--- handler.js
+++ handler.js
@@ -1,6 +1,45 @@
+const { google } = require('googleapis');
+const scopes = 'https://www.googleapis.com/auth/analytics.readonly';
+const jwt = new google.auth.JWT(process.env.GAPI_CLIENT_EMAIL, null, process.env.GAPI_PRIVATE_KEY, scopes);
+
 module.exports.query = async event => {
-  return {
-    statusCode: 200,
-    body: 'hello world',
-  };
+  const { _src, ...queryParams } = event.queryStringParameters;
+  try {
+    const rows = await new Promise((resolve, reject) => {
+      jwt.authorize(jwtError => {
+        if (!jwtError) {
+          google.analytics('v3').data.ga.get(
+            {
+              'auth': jwt,
+              ...queryParams
+            },
+            (gaError, gaResult) => {
+              if (!gaError)
+                resolve({
+                  statusCode: 200,
+                  body: JSON.stringify(gaResult.data.rows)
+                });
+              else
+                reject({
+                  statusCode: 400,
+                  body: JSON.stringify(gaError)
+                });
+            }
+          )
+        }
+        else {
+          reject({
+            statusCode: 400,
+            body: JSON.stringify(jwtError)
+          });
+        }
+      })
+    });
+    return rows;
+  } catch (rejectionError) {
+    console.log({rejectionError})
+    return rejectionError;
+  }
+ 
+
 };
